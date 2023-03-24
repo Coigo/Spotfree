455839601d6948a122e9594333594c5cfe952f9d
Luiz Miguel Ribeiro <codigo.TOLMR@gmail.com>
2023-03-24 18:30:03 -0300

    seccond one (i don't know what to write in a commit)

---
+++ README.md
@@ -0,0 +1,4 @@
+A try to copy spotfy
+
+i'm having a problem with the player, i'm tring to play audios directly in frontend
+maybe i'll have to create a api, i hope not honestly
---
+++ index.js
@@ -0,0 +1,13 @@
+const targetEl = document.querySelector('.Main')
+const load = pagina => {
+    fetch(`./templates/${pagina}.html`)
+        .then(res => {
+            if(res.ok){
+                return(res.text())
+            }
+        })
+        .then(htmlSnippet => {
+            targetEl.innerHTML = htmlSnippet
+        })
+}
+
